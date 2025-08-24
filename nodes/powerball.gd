6ea07691dbd610d53504a2178263a611a8c5007a
Gabriel <gabrielcancro@theogs.dev>
2025-08-24 19:20:09 -0300

    update

--- nodes/powerball.gd
+++ nodes/powerball.gd
@@ -1,7 +1,14 @@
 extends Node2D
 var PLAYER
+var time = 0
+var velocity = Vector2()
 func _ready():
 	PLAYER = get_node("/root/Game/player")
 
 func _process(delta: float) -> void:
-	position = PLAYER.position + Vector2(50,0)
+	time += delta
+	var distance = position.distance_to(PLAYER.position)
+	velocity += position.direction_to(PLAYER.position + Vector2(0,-80))*delta*distance*0.1
+	velocity *= 0.98
+	position += velocity
+	$Sprite2D.offset.y = sin(time*2)*5
